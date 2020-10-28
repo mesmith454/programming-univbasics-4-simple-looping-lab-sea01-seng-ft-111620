f1e26660cb95512857f997320d026b46625c4f78
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-28 18:11:08 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,36 @@
-# Write your methods here
+message = "Hello"
+limit = 5
+cats = ["snowshoe", "tabby", "bengal", "tuxedo"]
+
+def loop_message_five_times(message)
+  count = 0 
+  while count < 5
+    puts message
+    count += 1 
+  end
+end
+
+def loop_message_n_times(message, limit)
+  count = 0 
+  while count < limit do
+    puts message
+    count += 1 
+  end
+end
+
+def output_array(cats)
+  count = 0 
+  while count < cats.length do
+    puts cats[count]
+    count += 1 
+  end
+end
+
+def return_string_array(array)
+  count = 0
+  while count < array.length do
+    array[count] = array[count].to_s 
+    count += 1 
+  end
+  array
+end
